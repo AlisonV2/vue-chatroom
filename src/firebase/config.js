961fc75b6cbbbf7d1038c9d9291eff8b7341c223
AlisonV2 <alison.vandromme007@gmail.com>
2021-09-04 09:28:41 +0200

    firebase config setup

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,3 +1,6 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
 const firebaseConfig = {
     apiKey: process.env.VUE_APP_API_KEY,
     authDomain: process.env.VUE_APP_AUTH_DOMAIN,
@@ -6,3 +9,14 @@ const firebaseConfig = {
     messagingSenderId: process.env.VUE_APP_SENDER_ID,
     appId: process.env.VUE_APP_ID
 };
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+db.enablePersistence().catch((error) => {
+    console.log(`Firebase persistence error ${error.code}`);
+});
+
+export { db, timestamp };
